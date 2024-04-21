1e81092e83c1caf732b07e6c5beb6734c4d49f8a
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-04-22 00:18:57 +0200

    refactor: use next link in active link component

--- src/components/containers/ActiveLink/ActiveLink.tsx
+++ src/components/containers/ActiveLink/ActiveLink.tsx
@@ -1,28 +1,18 @@
 "use client";
 
+import NextLink from "next/link";
 import { Button } from "@/components/ui/button";
 import { type ActiveLinkTypeProps } from "@/components/containers/ActiveLink/types";
-import { useIsActiveLink } from "@/hooks/useIsActiveLink"; // Correct import of your Button component
+import { useIsActiveLink } from "@/hooks/useIsActiveLink";
 
 export const ActiveLink = ({ href, exact, children, ...props }: ActiveLinkTypeProps) => {
-	const matchedPath = (typeof href === "string" ? href : href.pathname) ?? null;
 	const isActive = useIsActiveLink({ href, exact });
 
-	const handleClick = () => {
-		if (typeof window !== "undefined") {
-			window.location.href = matchedPath;
-		}
-	};
-
 	return (
-		<Button
-			{...props}
-			variant={isActive ? "default" : "ghost"}
-			size={"sm"}
-			aria-current={isActive ? "page" : undefined}
-			onClick={handleClick}
-		>
-			{children}
+		<Button variant={isActive ? "default" : "ghost"} size={"sm"} asChild>
+			<NextLink {...props} href={href} aria-current={isActive ? "page" : undefined}>
+				{children}
+			</NextLink>
 		</Button>
 	);
 };
