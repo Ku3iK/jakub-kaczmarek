5accdfda315afb658d3c3aa2b4ef983ed4adb42a
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-04-21 16:03:02 +0200

    feat: display articles info

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,9 @@
+import { Articles } from "@/components/views/Articles";
+
+export default function ArticlesPage() {
+	return (
+		<main className="section-container w-full py-20">
+			<Articles />
+		</main>
+	);
+}
