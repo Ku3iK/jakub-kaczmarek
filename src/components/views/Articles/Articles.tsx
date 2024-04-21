548d7f71015e5f88de63c7176c5f72036ce5967d
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-04-21 16:02:37 +0200

    feat: create base structure for articles

---
+++ src/components/views/Articles/Articles.tsx
@@ -0,0 +1,16 @@
+import { TypographyH1, TypographyParagraph } from "@/components/views/Typography";
+
+export const Articles = () => {
+	return (
+		<section
+			aria-labelledby={"articles-heading"}
+			aria-describedby={"articles-description"}
+			className={"flex max-w-lg flex-col gap-6"}
+		>
+			<TypographyH1 id={"articles-heading"}>Articles</TypographyH1>
+			<TypographyParagraph id={"articles-description"}>
+				Stay tuned, coming soon...
+			</TypographyParagraph>
+		</section>
+	);
+};
