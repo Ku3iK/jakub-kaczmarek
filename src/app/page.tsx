02f645406962996d43e7de241ffe97cff8b1bbb2
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2024-04-21 15:45:47 +0200

    feat: create about me section (#5)
    * feat: create typography
    
    * feat: create about me section
    
    * feat: display about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { AboutMe } from "@/components/views/AboutMe";
+
 export default function Home() {
 	return (
 		<main className="section-container w-full py-20">
-			<h1>Jakub Kaczmarek</h1>
+			<AboutMe />
 		</main>
 	);
 }
