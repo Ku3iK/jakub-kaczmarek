34dd38eaa905554260315d50313419e80151f1bd
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-04-21 12:52:07 +0200

    feat: create constants for project

---
+++ src/constants/global.ts
@@ -0,0 +1,3 @@
+export const EMAIL_ADDRESS = "kubakaczmarek1102@gmail.com";
+
+export const CV_URL = "/jakub-kaczmarek-cv.pdf";
---
+++ src/constants/siteBranding.ts
@@ -0,0 +1,5 @@
+export const siteBranding = {
+	logotypeUrl: "/images/jakub-kaczmarek-memoi.png",
+	siteName: "Jakub Kaczmarek | jakubkaczmarek.dev",
+	name: "jakubkaczmarek.dev",
+};
